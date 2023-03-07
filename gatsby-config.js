980bc12ed1bb79990f900b12d30fd9289250bb14
jdjefferies3 <jdj30527@gmail.com>
2023-03-07 11:21:14 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `John Jefferies Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
